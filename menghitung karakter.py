891f3a067d7d0baf53c15a2fbeb9397dafd5f12f
Azis012 <111286224+Azis012@users.noreply.github.com>
2022-09-26 14:21:13 +0700

    Add files via upload

---
+++ menghitung karakter.py
@@ -0,0 +1,8 @@
+import time
+
+x=input("Masukan sebuah kalimat :")
+y=input("Karakter yang ingin dihitung jumlahnya :")
+print("Jumalah karakter {0} dalam kalimat tersebut ada {1}" .format(y, x.count(y)))
+
+
+time.sleep(50)
